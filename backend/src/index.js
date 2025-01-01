f35b3c1b51f8735425f289f6d88f8e0dcbb73f03
Divine <115173705+ApexPlayground@users.noreply.github.com>
2025-01-01 19:13:17 +0000

    feat: set up controllers and connected mongoDB to the application

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './lib/db.js';
+
 import authRoutes from './routes/auth.route.js';
 
+dotenv.config();
 const app = express();
+
+const port = process.env.PORT
 
 // routes for the app
 app.use("/api/auth", authRoutes);
 
-app.listen(5001, () => {
-    console.log('Server is running on port 5001');
+app.listen(port, () => {
+    console.log('Server is running on port: ' + port);
+    connectDB();
 });
