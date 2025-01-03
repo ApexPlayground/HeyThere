33f88d09eae2f15980f7ee9a366d91e305ed3ba8
Divine <115173705+ApexPlayground@users.noreply.github.com>
2025-01-03 11:18:50 +0000

    feat: implemented login, logout and signup functionality

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
-import { data } from "react-router-dom";
+
+import toast from "react-hot-toast";
 
 // Create a store to manage authentication state
 export const useAuthStore = create((set) => ({
@@ -30,6 +31,48 @@ export const useAuthStore = create((set) => ({
 
     // user signup
     signup: async (data) => {
+        set({ isSigningUp: true });
+        try {
+            const res = await axiosInstance.post("/auth/signup", data);
+            toast.success("Account created successfully");
 
+            // Set the authUser state with the user data
+            set({ authUser: res.data });
+
+        } catch (error) {
+            toast.error(error.response.data.message);
+
+        } finally {
+            set({ isSigningUp: false });
+        }
+
+    },
+
+    // user login
+    login: async (data) => {
+        set({ isLoggingIn: true });
+        try {
+            const res = await axiosInstance.post("/auth/login", data);
+            set({ authUser: res.data });
+            toast.success("Logged in successfully");
+
+
+        } catch (error) {
+            toast.error(error.response.data.message);
+        } finally {
+            set({ isLoggingIn: false });
+        }
+    },
+
+
+    // user logout
+    logout: async () => {
+        try {
+            await axiosInstance.post("/auth/logout");
+            set({ authUser: null });
+            toast.success("Logged out successfully");
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
     },
 }));
