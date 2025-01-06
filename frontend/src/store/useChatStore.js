498445ca612b4bfa3667d0505c3fba0bb2ebcf2e
Divine <115173705+ApexPlayground@users.noreply.github.com>
2025-01-06 14:32:53 +0000

    feat: users can sent and receive messages but not in real time

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -1,9 +1,10 @@
 import { create } from "zustand"
 import toast from "react-hot-toast"
 import { axiosInstance } from "../lib/axios"
-import { useAuthStore } from "./useAuthStore"
+import { useAuthStore } from "./useAuthStore";
 
-export const useChatStore = create((set) => ({
+
+export const useChatStore = create((set, get) => ({
     messages: [],
     users: [],
     selectedUser: null,
@@ -37,6 +38,16 @@ export const useChatStore = create((set) => ({
         }
     },
 
-    setSelectedUser: (selectedUser) => set({ selectedUser }),
+    sendMessage: async (messageData) => {
+        const { selectedUser, messages } = get();
+        try {
+            const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`, messageData);
+            set({ messages: [...messages, res.data] });
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
+    },
 
+
+    setSelectedUser: (selectedUser) => set({ selectedUser }),
 }))
