0800ef7551f18e360d5729c6d7c80dccb755deab
Divine <115173705+ApexPlayground@users.noreply.github.com>
2025-01-06 11:05:58 +0000

    feat: added themes from from daisyui to let users select different themes

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 const HomePage = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold text-center mt-10">Home Page</h1>
+      <h1 className="min-h-screen text-3xl font-bold text-center mt-10">Home Page</h1>
     </div>
   )
 }
